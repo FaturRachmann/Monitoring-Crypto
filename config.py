c27b1f53b5aefd6de3975f8241e3b10ddf7eaa81
Fatur Rachman <124031622+FaturRachmann@users.noreply.github.com>
2025-06-25 16:03:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config.py
 
 # WebSocket Alchemy untuk Ethereum TX real-time
-ALCHEMY_WEBSOCKET_URL = "wss://eth-mainnet.g.alchemy.com/v2/alcht_5vN3QwVHMVWNZ7GNV8LN5JPojOaIeo"
+ALCHEMY_WEBSOCKET_URL = "******"
 
 # (Opsional) API CoinGecko untuk harga
-COINGECKO_API = "https://api.coingecko.com/api/v3"
+COINGECKO_API = "******"
 
 # (Opsional) API key Groq (jika kamu mau gunakan summarizer AI)
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "*****"
 
-TELEGRAM_BOT_TOKEN = "8192919634:AAF2ICRJ95LO1nRzLarmlB3l0ZGJXeCQBwQ"
-TELEGRAM_CHAT_ID = "5724884196"
+TELEGRAM_BOT_TOKEN = "****"
+TELEGRAM_CHAT_ID = "****"
